e4c6a799e74804cca07d6689e2b204f9e5f6459e
farit <f.galeev@innopolis.ru>
2021-02-15 23:20:32 +0300

    + indexing and writting to mongodb

--- test.py
+++ test.py
@@ -1,21 +1,44 @@
 import requests
-from PIL import Image
 import base64
 import json
-import io
+import os
 
 if __name__ == '__main__':
-    with open("images/1.jpg", "rb") as f:
+    ####################################################################################################################
+    #                                         Test indexing of one image                                               #
+    ####################################################################################################################
+    with open("images/2.jpg", "rb") as f:
         image = f.read()
     # buffered = io.BytesIO()
     # image.save(buffered, format="JPEG")
     im = base64.b64encode(image)
     base64string = im.decode('utf-8')
     data = {
-        "img": base64string
+        "img": base64string,
+        "name": "2.jpg"
     }
     data = json.dumps(data, indent=2)
     r = requests.post("http://127.0.0.1:5000/encode/image", data=data)
+    assert r.status_code == 200
+
+    ####################################################################################################################
+    #                                        Test indexing of many images                                              #
+    ####################################################################################################################
+    image_base64_list = []
+    image_names = []
+    for img_name in os.listdir("images"):
+        with open(f"images/{img_name}", "rb") as f:
+            image = f.read()
+        im = base64.b64encode(image)
+        base64string = im.decode('utf-8')
+        image_names.append(img_name)
+        image_base64_list.append(base64string)
+    data = {
+        "img_list": image_base64_list,
+        "img_names": image_names
+    }
+    data = json.dumps(data, indent=2)
+    r = requests.post("http://127.0.0.1:5000/encode/images", data=data)
     assert r.status_code == 200
 
     data = {
